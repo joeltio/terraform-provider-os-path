6c3d3885531ec10d0d1f50500167e9f52e309a35
Joel Tio <joeltiojinhon@gmail.com>
2019-10-12 23:56:09 +0800

    Fix incorrect provider return type

--- ospath/provider.go
+++ ospath/provider.go
@@ -2,9 +2,10 @@ package ospath
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func Provider() *schema.Provider {
+func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 			"ospath_join": resourceJoin(),
