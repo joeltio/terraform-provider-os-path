a93e6bcac13cdfe4b399381d9a980fc6aa71c3e7
Joel Tio <joeltiojinhon@gmail.com>
2019-10-13 14:59:23 +0800

    Add missing update and delete

--- ospath/resource_join.go
+++ ospath/resource_join.go
@@ -9,7 +9,9 @@ import (
 func resourceJoin() *schema.Resource {
 	return &schema.Resource{
 		Create: createJoin,
-		Read:   schema.Noop,
+		Read:   readJoin,
+		Update: updateJoin,
+		Delete: schema.RemoveFromState,
 		Schema: map[string]*schema.Schema{
 			"path": &schema.Schema{
 				Type: schema.TypeList,
@@ -18,14 +20,39 @@ func resourceJoin() *schema.Resource {
 				},
 				Required: true,
 			},
+			"result": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
+func toStringSlice(interfaceSlice []interface{}) []string {
+	stringSlice := []string{}
+	for i := range interfaceSlice {
+		stringSlice = append(stringSlice, interfaceSlice[i].(string))
+	}
+
+	return stringSlice
+}
+
 func createJoin(d *schema.ResourceData, m interface{}) error {
-	pathArr := d.Get("path").([]string)
+	pathArr := toStringSlice(d.Get("path").([]interface{}))
 	joinedPath := path.Join(pathArr...)
 	d.Set("result", joinedPath)
 	d.SetId(joinedPath)
 	return nil
+}
+
+func readJoin(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func updateJoin(d *schema.ResourceData, m interface{}) error {
+	pathArr := toStringSlice(d.Get("path").([]interface{}))
+	joinedPath := path.Join(pathArr...)
+	d.Set("result", joinedPath)
+	d.SetId(joinedPath)
+	return readJoin(d, m)
 }
